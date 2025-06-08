7250e2fb8fdbd6a5da0aa954fa1eb0da958c5295
suriya <suriya.mechanical@gmail.com>
2025-06-08 13:42:45 +0200

    feat: add yellow feedback logic for misplaced letters

--- src/main/java/com/game/Wordle.java
+++ src/main/java/com/game/Wordle.java
@@ -8,9 +8,34 @@ public  class Wordle {
     }
 
     public String[] checkGuess(String guess) {
+
         String[] result = new String[5];
+        boolean[] used = new boolean[5];
+
+        // Check for  Green highlight
         for (int i = 0; i < 5; i++) {
-            result[i] = guess.charAt(i) == secret.charAt(i) ? "green" : "white";
+            if (guess.charAt(i) == secret.charAt(i)) {
+                result[i] = "green";
+                used[i] = true;
+            }
+        }
+
+        // Check for  Yellow & white highlight
+        for (int i = 0; i < 5; i++) {
+            if (result[i] == null) {
+                boolean found = false;
+                for (int j = 0; j < 5; j++) {
+                    if (!used[j] && guess.charAt(i) == secret.charAt(j)) {
+                        result[i] = "yellow";
+                        used[j] = true;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    result[i] = "white";
+                }
+            }
         }
         return result;
     }
