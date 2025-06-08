f1af3819f231a56eb00d496e13a8893c238c4e6f
suriya <suriya.mechanical@gmail.com>
2025-06-08 13:27:02 +0200

    test: add test for yellow match when letters are in wrong position

--- src/test/java/com/game/WordleTest.java
+++ src/test/java/com/game/WordleTest.java
@@ -12,4 +12,18 @@ public class WordleTest {
 
         assertArrayEquals(new String[]{"green", "green", "green", "green", "green"}, feedback);
     }
+
+    @Test
+    public void testLetterInWrongPositionIsYellow() {
+        Wordle wordle = new Wordle("apple");
+        String[] feedback = wordle.checkGuess("pleap");
+
+        // P is in wrong spot (yellow), L in wrong spot, etc.
+        assertEquals("yellow", feedback[0]);
+        assertEquals("yellow", feedback[1]);
+        assertEquals("yellow", feedback[2]);
+        assertEquals("yellow", feedback[3]);
+        assertEquals("yellow", feedback[4]);
+    }
+
 }
